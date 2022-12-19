24b74bc22b1f15233d4a133cd6a929f7007d46ee
Yisus <jesusgameroom@gmail.com>
2022-12-19 15:00:06 -0400

    Done under construction page
    Added new styles
    Added random messages

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    const messages = [
+        "Under Construction",
+        "Not ready yet!",
+        "Too early...",
+        "Working hard",
+        "Nope, not ready",
+        "Still waiting"
+    ];
+
+    function randomMessage(): string {
+        let randomIndex = Math.floor(Math.random() * messages.length);
+
+        return messages[randomIndex] ?? 'Under construction';
+    }
+
+    return (
+        <div className="under-construction">
+            <h1>{randomMessage()}</h1>
+            <p>This website is still under construction, come back later!</p>
+        </div>
+    );
 }
-
-export default App
