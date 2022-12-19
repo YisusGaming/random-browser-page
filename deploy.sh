1056142284574ea9b4e9599ac4df4f8684735eda
Yisus <jesusgameroom@gmail.com>
2022-12-19 15:22:54 -0400

    Added deploy command

--- deploy.sh
+++ deploy.sh
@@ -18,4 +18,6 @@ git commit -m "deploy"
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:YisusGaming/random-browser.git main:gh-pages
+git push -u -f git@github.com:YisusGaming/random-browser.git main:gh-pages
+
+cd -
