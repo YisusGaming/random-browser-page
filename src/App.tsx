f9efdfc9068e015ff0cac7a1106458e5c21bfc4b
Yisus <jesusgameroom@gmail.com>
2023-01-03 05:28:26 -0400

    Latest version brand done

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,9 @@ export default function App() {
     return (
         <>
             <Navbar />
-            <div className="brand-wrap">
-                <div className="latest-brand">
-                    <h1>1.2.0 is here!</h1>
-                    <a target='_blank' href="https://github.com/YisusGaming/random-browser/releases/latest">Download {"->"}</a>
-                </div>
+            <div className="latest-brand">
+                <h1><span id="current-version">1.2.0</span> is here!</h1>
+                <a target='_blank' href="https://github.com/YisusGaming/random-browser/releases/latest">Download {"->"}</a>
             </div>
         </>
     );
