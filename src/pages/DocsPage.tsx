b66f54c4a339003fa08a7a19e1b7ceb6fc08dd70
Yisus <jesusgameroom@gmail.com>
2023-01-03 06:34:25 -0400

    Done 'still working' message in docs page

--- src/pages/DocsPage.tsx
+++ src/pages/DocsPage.tsx
@@ -1,10 +1,14 @@
 import Navbar from "../components/Navbar";
+import './css/docsPage.css';
 
 export default function DocsPage() {
     return (
         <>
             <Navbar />
-            <h1>Documentation</h1>
+            <div className="working-on-docs">
+                <h1>Hey! I'm still working on Random Browser's documentation...</h1>
+                <p>Keep in mind that I'm just a <strong>random</strong> guy working on a <strong>random</strong> browser...</p>
+            </div>
         </>
     );
 }
